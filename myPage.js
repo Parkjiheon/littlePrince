a1290ef2561dc34e84a4591840b45056f8a884ad
Parkjiheon <65212877+Parkjiheon@users.noreply.github.com>
2022-07-11 16:00:42 +0900

    Add files via upload

---
+++ myPage.js
@@ -0,0 +1,22 @@
+
+// References to DOM Elements
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+const list = document.querySelectorAll('.list');
+
+
+
+//menuToggle
+menuToggle.onclick = () => {
+    navigation.classList.toggle('open')
+}
+
+//click menu
+function activeLink(){
+    list.forEach((item) => 
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) => 
+item.addEventListener('click', activeLink));
+
